4cac84923f6647969fad1dfd5fb143825346be00
Nelson Carvalho <nelson.montagevideo@gmail.com>
2025-02-28 13:54:03 +0000

    example lecture reboot

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,5 +1,4 @@
-SELECT 
-*
+SELECT *
 FROM {{ ref('stg_raw__sales') }} AS sales
 LEFT JOIN {{ ref('stg_raw__product') }} AS pdt
-    ON sales.product_id = pdt.products_id
+    ON sales.pdt_id = pdt.products_id
